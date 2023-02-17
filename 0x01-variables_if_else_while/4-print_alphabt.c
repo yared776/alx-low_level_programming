a1662949a42bdf1cb5516ea425a764a190decef7
Yared A. Beriso <yared776philo@gmail.com>
2023-02-17 13:35:16 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is alphabet except q and e.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
