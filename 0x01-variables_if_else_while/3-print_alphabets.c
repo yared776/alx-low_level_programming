a6ee767f1188182eac20d627473d32769d22267d
Yared A. Beriso <yared776philo@gmail.com>
2023-02-17 13:11:03 -0800

    for(i=a;i<=z;i++)     {         putchar(i); exit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - This is the alphabet aA
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
