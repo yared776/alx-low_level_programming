ea7015d00e841fc5de136a528fe962fdf0f09dc0
Yared A. Beriso <yared776philo@gmail.com>
2023-02-22 05:25:18 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet X10
+ *
+ * Return: 0 is cool
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+			_putchar('\n');
+			i++;
+		}
+	}
+}
