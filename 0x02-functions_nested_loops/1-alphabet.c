bcbae92358d42212714c7cbc8519ab79d581d1fb
Yared A. Beriso <yared776philo@gmail.com>
2023-02-22 00:38:53 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: 0 is the best :)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
